f986565766ddc212737be1b054ba887c439ee7fc
Kamal Hassan <scienceknowledge191andme@gmail.com>
2026-06-05 02:52:32 +0500

    react-confetti and refactoring

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import { useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import Die from "./components/Die";
+import ReactConfetti from "react-confetti";
 
 function generateRandomNum() {
 	return Math.floor(Math.random() * 6) + 1;
 }
-
 function generateDiceArray() {
 	return Array.from({ length: 10 }, () => {
 		return {
@@ -15,12 +15,16 @@ function generateDiceArray() {
 }
 
 export default function App() {
-	const [dice, setDice] = useState(generateDiceArray());
+	const [dice, setDice] = useState(generateDiceArray);
 
-	function gameWonStatus() {
-		return dice.every((die) => die.isHeld) && dice.every((die) => die.value === dice[0].value);
-	}
-	const gameWon = gameWonStatus();
+	const newGameBtnRef = useRef(null);
+	const gameWon = dice.every((die) => die.isHeld) && dice.every((die) => die.value === dice[0].value);
+
+	useEffect(() => {
+		if (gameWon) {
+			newGameBtnRef.current?.focus();
+		}
+	}, [gameWon]);
 
 	//handlers
 	function handleRoll() {
@@ -35,6 +39,7 @@ export default function App() {
 
 	return (
 		<main className="h-full w-full flex flex-col items-center justify-evenly bg-[#F5F5F5] rounded-xl max-h-100 max-w-100 sm:max-h-150 sm:max-w-150 p-4">
+			{gameWon && <ReactConfetti />}
 			<section>
 				<h1 className="text-center tracking-tighter text-[2.5rem] font-bold text-[#2B283A]">Tenzies</h1>
 				<p className="text-center tracking-tight font-normal text-[#4A4E74]">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
@@ -51,6 +56,7 @@ export default function App() {
 			{gameWon ? (
 				<button
 					className="h-12 w-28 text-[1.2rem] bg-[#5035FF] rounded-md cursor-pointer text-white p-1.5"
+					ref={newGameBtnRef}
 					onClick={handleNewGame}>
 					New Game
 				</button>
