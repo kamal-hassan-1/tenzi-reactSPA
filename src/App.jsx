cb010040ea0f382e973a4f751d3f36f836d6ea1b
Kamal Hassan <scienceknowledge191andme@gmail.com>
2026-06-04 18:02:51 +0500

    new func.

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,39 @@
+import { useState } from "react";
 import Die from "./components/Die";
 
 function generateDiceArray() {
-	return Array.from({ length: 10 }, () => Math.floor(Math.random() * 6) + 1);
+	return Array.from({ length: 10 }, () => {
+		return {
+			value: Math.floor(Math.random() * 6) + 1,
+			isHeld: false,
+		};
+	});
 }
 
 export default function App() {
-	const dice = generateDiceArray();
+	console.log("rendered");
+
+	const [dice, setDice] = useState(generateDiceArray());
+
+	function handleRoll() {
+		setDice(generateDiceArray());
+	}
 
 	return (
-		<main className="h-full w-full flex items-center justify-center bg-white rounded-xl max-h-100 max-w-100 sm:max-h-150 sm:max-w-150 p-4">
+		<main className="h-full w-full flex flex-col items-center justify-evenly bg-white rounded-xl max-h-100 max-w-100 sm:max-h-150 sm:max-w-150 p-4">
 			<section className="grid grid-cols-5 gap-6">
 				{dice.map((num, idx) => (
 					<Die
 						key={idx}
-						num={num}
+						obj={num}
 					/>
 				))}
 			</section>
+			<button
+				className="h-12 w-24 text-[1.2rem] bg-[#5035FF] rounded-md cursor-pointer text-white p-1.5"
+				onClick={handleRoll}>
+				Roll
+			</button>
 		</main>
 	);
 }
--- src/components/Die.jsx
+++ src/components/Die.jsx
@@ -1,9 +1,12 @@
 export default function Die(props) {
 	return (
 		<button
-			className="h-10 w-10 sm:w-16 sm:h-16 rounded-lg bg-white text-[#2B283A] font-bold text-2xl sm:text-4xl cursor-pointer"
-			style={{ boxShadow: "0 10px 25px rgba(0, 0, 0, 0.2)" }}>
-			{props.num}
+			className="h-10 w-10 sm:w-16 sm:h-16 rounded-lg bg-white text-[#2B283A] font-karla font-bold text-2xl sm:text-4xl cursor-pointer"
+			style={{
+				backgroundColor: props.obj.isHeld ? "#59E391" : "#FFFFFF",
+				boxShadow: "0 10px 25px rgba(0, 0, 0, 0.2)",
+			}}>
+			{props.obj.value}
 		</button>
 	);
 }
