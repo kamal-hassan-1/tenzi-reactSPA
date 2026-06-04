0ad5a5d9c442efeb669061c5f0501d24fc6d1be5
Kamal Hassan <scienceknowledge191andme@gmail.com>
2026-06-04 16:34:45 +0500

    early setup of dice with random nums

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,21 @@
 import Die from "./components/Die";
+
+function generateDiceArray() {
+	return Array.from({ length: 10 }, () => Math.floor(Math.random() * 6) + 1);
+}
+
 export default function App() {
+	const dice = generateDiceArray();
+
 	return (
 		<main className="h-full w-full flex items-center justify-center bg-white rounded-xl max-h-100 max-w-100 sm:max-h-150 sm:max-w-150 p-4">
 			<section className="grid grid-cols-5 gap-6">
-				<Die num={1} />
-				<Die num={1} />
-				<Die num={1} />
-				<Die num={1} />
-				<Die num={1} />
-				<Die num={1} />
-				<Die num={1} />
-				<Die num={1} />
-				<Die num={1} />
-				<Die num={1} />
+				{dice.map((num, idx) => (
+					<Die
+						key={idx}
+						num={num}
+					/>
+				))}
 			</section>
 		</main>
 	);
--- src/components/Die.jsx
+++ src/components/Die.jsx
@@ -1,7 +1,7 @@
 export default function Die(props) {
 	return (
 		<button
-			className="w-16 h-16 rounded-lg bg-white text-[#2B283A] font-bold text-4xl cursor-pointer"
+			className="h-10 w-10 sm:w-16 sm:h-16 rounded-lg bg-white text-[#2B283A] font-bold text-2xl sm:text-4xl cursor-pointer"
 			style={{ boxShadow: "0 10px 25px rgba(0, 0, 0, 0.2)" }}>
 			{props.num}
 		</button>
